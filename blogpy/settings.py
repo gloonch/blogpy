2681ed0b4d7511bfb44a041db918fc4e472f3bcd
mahdihadian <mahdihadian@hotmail.com>
2020-04-27 00:55:55 +0430

    template loaded

--- blogpy/settings.py
+++ blogpy/settings.py
@@ -124,8 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-# STATIC_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # CKEDITOR
 # CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor"
--- blogpy/urls.py
+++ blogpy/urls.py
@@ -19,5 +19,8 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^', include('blog.urls')),
     # url(r'^ckeditor/', include('ckeditor_uploader.urls')),
+
 ]
+
