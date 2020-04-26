d70d486a9dfe4be7fd020897196ae5180e85a1cb
mahdihadian <mahdihadian@hotmail.com>
2020-04-26 23:51:52 +0430

    added requirements.txt and AdminModel classes

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(UserProfile)
-admin.site.register(Article)
-admin.site.register(Category)
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'avatar', 'description']
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'created_at']
+    search_fields = ['title', 'content']
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'cover']
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Category, CategoryAdmin)
