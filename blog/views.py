126280778cff3d9f2496ce1b5e791fc42ee01cd7
mahdihadian <mahdihadian@hotmail.com>
2020-04-29 10:25:33 +0430

    DRF API : article/all/ added

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import *
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class IndexPage(TemplateView):
@@ -15,8 +18,48 @@ class IndexPage(TemplateView):
                 'category': article.category.title,
                 'created_at': article.created_at.date,
             })
-        context = {
-            "article_data": article_data
-        }
+        promote_data = []
+        all_promote_articles = Article.objects.filter(promote=True)
+        for promote_article in all_promote_articles:
+            promote_data.append({
+                'category': promote_article.category.title,
+                'title': promote_article.title,
+                'author': promote_article.author.user.first_name + ' ' + promote_article.author.user.first_name,
+                'avatar': promote_article.author.avatar.url if promote_article.author.avatar else None,
+                'created_at': promote_article.created_at.date(),
+                'cover': promote_article.cover.url if promote_article.cover else None,
 
-        return render(request, "index.html", context)
+            })
+            context = {
+                "article_data": article_data,
+                'promote_article_data': promote_data,
+            }
+
+            return render(request, "index.html", context)
+
+
+class ContactPage(TemplateView):
+    template_name = 'page-contact.html'
+
+
+class AllArticleAPIView(APIView):
+    def get(self, request, format=None):
+        try:
+            all_article = Article.objects.all().order_by('-created_at')
+            data = []
+
+            for article in all_article:
+                data.append({
+                    'title': article.title,
+                    'cover': article.cover.url if article.cover else None,
+                    'content': article.content,
+                    'created_at': article.created_at,
+                    'category': article.category.title,
+                    'author': article.author.user.first_name + ' ' + article.author.user.last_name,
+                    'promote': article.promote,
+                })
+
+            return Response({'data': data }, status=status.HTTP_200_OK)
+        except:
+            return Response({'status': "Internal Server Error, We'll check it later "},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
--- blogpy/urls.py
+++ blogpy/urls.py
@@ -22,10 +22,10 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^', include('blog.urls')),
-    # url(r'^ckeditor/', include('ckeditor_uploader.urls')),
+    url(r'^api-auth/', include('rest_framework.urls')),
 
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static('/contact/static/', document_root=settings.STATIC_ROOT)
